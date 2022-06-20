5ee339f192e9c876316c4a6e2deb2971e9c8689d
Weblymit <weblymit@gmail.com>
2022-06-20 10:57:54 +0200

    delte and done ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.getElementById("push").onclick = function () {
+document.getElementById("push").onclick = function (e) {
 	let elInput = document.getElementById("todo-input").value;
 	// condition validate form
 	//Si le champ d'input est vide
@@ -12,13 +12,30 @@ document.getElementById("push").onclick = function () {
 	} else if (elInput.length > 35) {
 		alert("Tache trop longue");
 	} else {
-		console.log("Tout est Ok");
-		// selectionne la div ou les taches vont être affichéés
+
+		// Ajouter une tâche
 		document.querySelector("#tasks").innerHTML += `
-    <div class="d-flex justify-content-between align-items-center mb-3">
-      <span class="my-0">${elInput}</span>
-      <button class="btn btn-danger">Delete</button>
+    <div class="d-flex justify-content-between align-items-center mb-3 border-bottom py-3">
+      <span class="my-0 taskname">${elInput}</span>
+      <button class="btn btn-danger delete">Delete</button>
     </div>
     `;
+
+		// delete task
+		let tasksDelete = document.querySelectorAll(".delete");
+		for (let i = 0; i < tasksDelete.length; i++) {
+			tasksDelete[i].onclick = function () {
+				this.parentNode.remove();
+			};
+		}
+		// task done
+		let tasksDone = document.querySelectorAll(".taskname");
+		for (let i = 0; i < tasksDone.length; i++) {
+			tasksDone[i].onclick = function () {
+				this.classList.toggle("completed");
+			};
+		}
+
+		document.querySelector("#todo-input").value = "";
 	}
 };
